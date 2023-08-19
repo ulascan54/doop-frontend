c503c7135d0860509aadcd0ed7d8885bbc16a382
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-08-19 17:10:18 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button"
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant="contained">Hello world</Button>
+    </div>
+  )
+}
+
+export default App
